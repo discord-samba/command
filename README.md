e9764ed2ced11d11a875509fa7fc7c19fe4be634
zajrik <zajrik@users.noreply.github.com>
2019-11-12 23:08:32 +0000

    Build docs from master: 88c341fe9e333a71a013a3e168ab6d1b6f55276f

--- README.md
+++
@@ -1,6 +0,0 @@
-## Discord-Sambo Command Module
-This is the command module for Discord-Sambo. It provides a command system for a Discord.js (or Discord-Sambo core framework) Client
-
-# NOT READY FOR USE
-
-TODO: Full write-up on the module
--- src/types/CommandArgumentSpecEntry.ts
+++
@@ -1,7 +0,0 @@
-import { CommandArgumentKind } from './CommandArgumentKind';
-
-/**
- * Represents any value retrieved from `CommandArgumentSpec`
- * @private
- */
-export type CommandArgumentSpecEntry = { kind: CommandArgumentKind } | undefined;
