dac35882db4cfab45a58ce38a864e01b0567ccfe
Zack Campbell <zajrik@users.noreply.github.com>
2021-02-23 11:44:32 -0600

    docs: update NextFunction docs

--- src/types/NextFunction.ts
+++ src/types/NextFunction.ts
@@ -5,6 +5,6 @@ import { Result } from '#root/Result';
  * that will call the next Rule/Middleware in sequence to continue execution. Will
  * cancel execution if given [[`Result.cancel | Result.cancel()`]] or [[`Result.err
  * | Result.err()`]]. [[`Result.ok | Result.ok()`]] will allow Rules/Middleware/dispatch
- * to continue. If no `Result` is passed, will default to `Result.ok()`
+ * to continue. Defaults to `Result.ok()` if no `Result` is passed
  */
 export type NextFunction = (result?: Result) => void;
