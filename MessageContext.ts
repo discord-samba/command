46d95283369e35d86cead059eb0fa89dfb548c54
zajrik <zajrik@users.noreply.github.com>
2021-02-23 20:22:39 +0000

    Push staging from: 2d7ff637e4993add85a11686e92ace4dcd678ec6

--- MessageContext.ts
+++ MessageContext.ts
@@ -1,8 +1,8 @@
 import { Client, Message } from 'discord.js';
 
 /**
- * Context for a sent Message. This context will be given to rules to help determine
- * if a command should be run
+ * Context for a sent Message. This context will be given to rules to be used to
+ * determine if a command should be run
  */
 export class MessageContext<T extends Client = Client>
 {
@@ -17,15 +17,18 @@ export class MessageContext<T extends Client = Client>
 	public message: Message;
 
 	/**
-	 * The content of the message that will be parsed for the command
-	 * and arguments. This can be modified by rules to affect what will
-	 * be parsed (to strip a prefix, for example)
+	 * The content of the message that will be parsed for the command and arguments.
+	 * This can be modified by rules to affect what will be parsed (to strip a
+	 * prefix, for example). Assuming a regex or function trigger does not trigger
+	 * a command, the first string of characters terminated by any whitespace will
+	 * be parsed as a command identifier, and the remaining (trimmed) content will
+	 * be parsed as arguments
 	 */
 	public content: string;
 
 	/**
 	 * Whether or not a prefix was used. Should be set by rules that check
-	 * for prefixes (which would then strip the used prefix from
+	 * for prefixes (which should then strip the used prefix from
 	 * [[`MessageContext.content | content`]])
 	 */
 	public prefixUsed: boolean;
